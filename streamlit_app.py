7761916b6fdb5afdfe1a887722b7f16d368a4022
Nishanth1254 <103844871+Nishanth1254@users.noreply.github.com>
2023-04-28 02:46:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit 
-import pandas
-import snowflake.connector
-import requests
-from urllib.error import URLError
 
 streamlit.title('My Parent New Healthy Diner')
 
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
