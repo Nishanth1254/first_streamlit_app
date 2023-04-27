d10b311673cfa69bca7421dd04b3a12ff54cc5eb
Nishanth1254 <103844871+Nishanth1254@users.noreply.github.com>
2023-04-27 22:55:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwiki")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
